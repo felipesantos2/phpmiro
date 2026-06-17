37dc988320907edcf8294c1246c80f30e8fe892d
felipesantos2 <santospinheiro627@gmail.com>
2026-06-17 11:27:05 -0300

    feat: start design of the routing feature

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,44 @@
 
 declare(strict_types=1);
 
-use League\Route\Router;
+use app\controllers\WelcomeController;
 
-$router = new Router();
+dd(
+    $_SERVER['REQUEST_URI'],
+    $_SERVER['REQUEST_METHOD'],
+    $_SERVER['HTTP_HOST'],
+    $_SERVER['HTTP_ACCEPT'],
+    $_SERVER['QUERY_STRING'],
+    $_SERVER['HTTP_HOST']
+);
 
-$router->get('/', 'UserController::index');
+get('/', function() {
+
+});
+
+get('/welcome', [WelcomeController::class, 'index']);
+
+post('/', function() {
+
+});
+
+function get($path, Closure|string|array $callback)
+{
+    return $callback();
+}
+
+function post($path, Closure|string|array $callback)
+{
+    if( is_callable($callback) ) {
+        return $callback();
+    }
+
+    if( is_string($callback) ) {
+        return call_user_func($callback);
+    }
+
+    if( is_array($callback) ) {
+        return call_user_func_array($callback);
+    }
+}
+
